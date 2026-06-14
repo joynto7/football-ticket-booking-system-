6d10cd6d46ae6e9a7399945505196aa69bd193eb
joyntoghosh <joyntoghosh7@gmail.com>
2026-06-14 21:41:27 +0600

    Query4 and query5 added

--- query.sql
+++ query.sql
@@ -96,3 +96,18 @@ WHERE full_name ILIKE 'Tanvir%'
        COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM Bookings
 WHERE payment_status IS NULL;
+
+SELECT b.booking_id,
+       u.full_name,
+       m.fixture,
+       b.total_cost
+FROM Bookings b
+INNER JOIN Users   u ON b.user_id  = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
+
+SELECT u.user_id,
+       u.full_name,
+       b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
