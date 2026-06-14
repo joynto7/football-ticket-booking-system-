0b3a9d098dc97221e49fcbf5c1dfbf9e1db0affd
joyntoghosh <joyntoghosh7@gmail.com>
2026-06-14 21:35:35 +0600

    Inserting Data on Bookings

--- query.sql
+++ query.sql
@@ -72,4 +72,10 @@ INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, 
 (105, 'Juventus vs Roma',         'Serie A',           80.00, 'Available');
 
 
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL,   NULL,        150.00),
+(505, 3, 102, 'C-20', 'Pending',   120.00);
 
